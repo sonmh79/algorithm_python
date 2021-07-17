c2221f1744d85d0adcb33d39523438600a47f2ed
minhyeok <sonmh79@naver.com>
2021-07-18 01:47:59 +0900

    wtffffffffffffffffffffffffffffff

---
+++ Greedy/Task_Schduler.py
@@ -0,0 +1,23 @@
+import collections
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        counter = collections.Counter(tasks)
+        result = 0
+        
+        while True:
+            sub_count = 0
+            
+            for task, _ in counter.most_common(n+1):
+                sub_count += 1
+                result += 1
+                
+                counter.subtract(task)
+                # 0 이하인 아이템을 목록에서 완전히 제거
+                counter += collections.Counter()
+                
+            if not counter:
+                break
+            
+            result += n - sub_count + 1
+            
+        return result
