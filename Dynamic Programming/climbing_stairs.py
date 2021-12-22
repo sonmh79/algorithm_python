b8e0d6986939692279d97bc4a5e8538baf05d1a4
Minhyeok <sonmh79@naver.com>
2021-12-22 16:01:56 +0900

    Modified Code

--- Dynamic Programming/climbing_stairs.py
+++ Dynamic Programming/climbing_stairs.py
@@ -1,16 +1,38 @@
-
-def climbStairs(n) -> int:
+class Solution:
+    
+    """ 1.Tabulation """
+    
+    def climbStairs(self, n: int) -> int:
+        
+        if n == 1 or n == 2:
+            return n
+        
         d = [0] * (n+1)
         d[1] = 1
         d[2] = 2
-        print(d)
-        
-        if n == 1 or 2:
-            return d[n]
         
         for i in range(3,n+1):
             d[i] = d[i-1] + d[i-2]
         
-        print(d)
         return d[-1]
-print(climbStairs(3))
+
+from collections import defaultdict
+
+class Solution:
+    
+    """ 2. Memoization """
+    
+    d = defaultdict(int)
+    
+    def climbStairs(self, n: int) -> int:
+        
+        if n <= 2:
+            return n
+        
+        if self.d[n]: # 계산 결과 저장
+            return self.d[n]
+        
+        self.d[n] = self.climbStairs(n-1) + self.climbStairs(n-2)
+        return self.d[n]
+        
+        
