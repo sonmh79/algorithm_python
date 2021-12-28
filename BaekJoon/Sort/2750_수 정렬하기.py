80d10eba8b6fab0011f3a7fc044a8ef1232b3a23
Minhyeok <sonmh79@naver.com>
2021-12-28 16:14:45 +0900

    Quick Sort

--- BaekJoon/Sort/2750_수 정렬하기.py
+++ BaekJoon/Sort/2750_수 정렬하기.py
@@ -16,5 +16,27 @@ for _ in range(n):
 #         if l[j] > l[j+1]:
 #             l[j],l[j+1] = l[j+1],l[j]
 
+# 3. 퀵 소트 O(nlogn) ~ O(n^2)
+# def quick_sort(array):
+#     if len(array) <= 1:
+#         return array
+    
+#     pivot = len(array) // 2
+
+#     less, equal, more = [], [], []
+
+#     p = array[pivot]
+#     for i in range(len(array)):
+#         if array[i] > p:
+#             more.append(array[i])
+#         elif array[i] == p:
+#             equal.append(array[i])
+#         else:
+#             less.append(array[i])
+    
+#     return quick_sort(less) + equal + quick_sort(more)
+
+# l = quick_sort(l)
+
 for num in l:
     print(num)
