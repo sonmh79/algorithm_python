6f65f504bc61e372b8e1f1578c237526802eb916
Minhyeok <sonmh79@naver.com>
2022-06-07 17:31:10 +0900

    BFS: *

---
+++ BaekJoon/그래프와 순회/24444_알고리즘 수업 - 너비 우선 탐색 1.py
@@ -0,0 +1,29 @@
+import sys
+from collections import defaultdict, deque
+input = sys.stdin.readline
+
+g = defaultdict(list)
+N,M,R = map(int,input().split())
+for _ in range(M):
+    a,b = map(int,input().split())
+    g[a].append(b)
+    g[b].append(a)
+
+ans = [0] * (N+1)
+cnt = 1
+visited = defaultdict(int)
+visited[R] = 1
+ans[R] = 1
+
+q = deque([R])
+while q:
+    node = q.popleft()
+    for n in sorted(g[node]):
+        if not visited[n]:
+            visited[n] = 1
+            cnt += 1
+            ans[n] = cnt
+            q.append(n)
+
+for i in range(1,N+1):
+    print(ans[i])
