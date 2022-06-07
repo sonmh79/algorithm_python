a0720c1545f9f14a5f160e632f25e0f65bb3a82a
Minhyeok <sonmh79@naver.com>
2022-06-07 17:23:49 +0900

    DFS: *

---
+++ BaekJoon/그래프와 순회/24480_알고리즘 수업 - 깊이 우선 탐색 2.py
@@ -0,0 +1,31 @@
+import sys
+from collections import defaultdict
+
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
+N,M,R = map(int,input().split())
+g = defaultdict(list)
+
+for _ in range(M):
+    a,b = map(int,input().split())
+    g[a].append(b)
+    g[b].append(a)
+
+ans = [0] * (N+1)
+visited = defaultdict(int)
+visited[R] = 1
+ans[R] = 1
+cnt = 1
+def dfs(n):
+
+    for node in sorted(g[n],reverse=True):
+        if not visited[node]:
+            visited[node] = 1
+            global cnt
+            cnt += 1
+            ans[node] = cnt
+            dfs(node)
+dfs(R)
+for i in range(1,N+1):
+    print(ans[i])
